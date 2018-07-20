fe2820c20e4c9391b4df972f3f4010347770139f
Olivera <jo14583@nsroot.net>
2018-07-20 19:30:31 -0300

    Initial commit
    Co-Authored-By: Juan Olivera <juam17@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
+import { CallbackComponent } from './callback.component';
+import { PublicDealsComponent } from './public-deals/public-deals.component';
+import { PrivateDealsComponent } from './private-deals/private-deals.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+      path: '',
+      redirectTo: 'deals',
+      pathMatch: 'full'
+    },
+    {
+      path: 'deals',
+      component: PublicDealsComponent
+    },
+    {
+      path: 'special',
+      component: PrivateDealsComponent,
+      // Add this to guard this route
+      canActivate: [
+        AuthGuard
+      ]
+    },
+    {
+      path: 'callback',
+      component: CallbackComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  auth: {
+    clientID: 'oiWQt0KdZEDLSPHSqhDqqRNoeBOCtv3R',
+    domain: 'jp-olivo.auth0.com', // e.g., you.auth0.com
+    audience: 'http://localhost:8080', // e.g., http://localhost:3001
+    redirect: 'http://localhost:4200/callback',
+    scope: 'openid profile email read:deals'
+  }
 };
 
 /*
