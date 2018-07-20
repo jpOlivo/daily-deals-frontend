bb5ab62467015c146210733066c70a7709a6546d
Olivera <jo14583@nsroot.net>
2018-07-20 19:26:02 -0300

    Initial commit
    Co-Authored-By: Juan Olivera <juam17@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div class="container">
+      <nav class="navbar navbar-default">
+        <div class="navbar-header">
+          <a class="navbar-brand" routerLink="/"></a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li>
+            <a class="navbar-brand" routerLink="/deals" routerLinkActive="active">Deals</a>
+          </li>
+          <li>
+            <a class="navbar-brand" routerLink="/special" *ngIf="authService.isLoggedIn" routerLinkActive="active">Private Deals</a>
+          </li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <a *ngIf="!authService.isLoggedIn" (click)="authService.login()">Log In</a>
+          </li>
+          <li>
+            <a *ngIf="authService.isLoggedIn" (click)="authService.logout()">Log Out</a>
+          </li>
+        </ul>
+      </nav>
+      <div class="col-sm-12">
+        <router-outlet></router-outlet>
+      </div>
+    </div>
+  `,
+  styles: [
+    `.navbar-right { margin-right: 0px !important}`
+  ]
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Daily Deals';
+  constructor(public authService: AuthService) {}
 }
